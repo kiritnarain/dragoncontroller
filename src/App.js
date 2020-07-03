ff051d048606a6f621a102c450ddf13d14611e12
kiritnarain <kiritnarain@gmail.com>
2020-07-04 01:09:14 +0530

    Fixed same-origin browser issue by using a proxy.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 import Controls from "./Controls";
 
 class App extends React.Component{
-    BACKEND_IP = "http://kiritnarain.com/dragonbackend";
+    //BACKEND_IP = "http://kiritnarain.com/dragonbackend";
+    //BACKEND_IP = "http://localhost:5091";
+
 
     constructor(props) {
         super(props);
@@ -23,15 +25,37 @@ class App extends React.Component{
     }
 
     fetchIP(){
-        fetch(this.BACKEND_IP+'//getIP')
+        /*fetch('/dragonbackend/getIP') //Uses backend defined in proxy
             .then(response => {
-                console.log('Got Dragon IP: '+response);
+                console.log('Got Dragon IP: '+response.body);
                 if(response!=="0.0.0.0"){
                     this.setState({
-                        serverIP: response
+                        serverIP: response.body
                     });
                 }
-            });
+            });*/
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = () => {
+        if(xhttp.readyState === XMLHttpRequest.DONE) {
+            var status = xhttp.status;
+            if (status === 0 || (status >= 200 && status < 400)) {
+                // The request has been completed successfully
+                var ip = xhttp.responseText;
+                console.log('Got Dragon IP: '+ip);
+                if(ip!=="0.0.0.0"){
+                    this.setState({
+                        serverIP: ip
+                    });
+                }
+            } else {
+                // Oh no! There has been an error with the request!
+                console.log('Error: '+status)
+            }
+        }
+
+        };
+        xhttp.open("GET", '/dragonbackend/getIP', true);
+        xhttp.send();
     }
 
 
